56960f01c556c46a6ee7c79878dd2d1cb5ea2ff4
Stanley Nwogu <stanleynwogu37@gmail.com>
2024-06-10 02:27:43 -1200

    first edit on about hero component done

---
+++ client/src/components/homepageAbout/HomePageAbout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./HomePageAbout.css";
+
+const HomePageAbout = () => {
+	return (
+		<div className="HomePageAbout">
+			<div className="h-about-content">
+				<span>about the tournament</span>
+				<span>
+					The Coal City Cup is an annual international pre-season football
+					tournament hosted at The Nnamdi Azikiwe Stadium in Enugu, Nigeria. The
+					first edition of the 11-day competition will hold from August 1-11,
+					2024. Rangers International Football Club of Enugu is a permanent
+					feature as the host team. The 2024 competition will feature 4 teams
+					from the NPFL and 4 teams from outside the country.
+				</span>
+				<span>read more </span>
+			</div>
+		</div>
+	);
+};
+
+export default HomePageAbout;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
 import Mission from "../components/mission/Mission";
+import HomePageAbout from "../components/homepageAbout/HomePageAbout";
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
 			<Mission />
+			<HomePageAbout />
 		</>
 	);
 };
